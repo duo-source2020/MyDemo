acbc63ced69f93402c88c2d14b198bb54f298c5d
Christina.huang <Christina.huang@halliburton.com>
2020-09-04 12:33:29 -0500

    Add delegate

--- DelegateMain/Program.cs
+++ DelegateMain/Program.cs
@@ -4,6 +4,8 @@ namespace DelegateMain
 {	
 	class Program
 	{
+		public delegate int TwoIntegerOperation(int par1, int par2);
+
 		static void Main(string[] args)
 		{
 			Console.WriteLine("please enter two integers");
@@ -11,9 +13,17 @@ namespace DelegateMain
 			var firstInt = int.Parse(Console.ReadLine());
 			var secondInt = int.Parse(Console.ReadLine());
 
-			Console.WriteLine("Add result: {0}", Add(firstInt, secondInt));
-			Console.WriteLine("Multiply result: {0}", Multiply(firstInt, secondInt));
-			Console.WriteLine("Union result: {0}", Union(firstInt, secondInt));
+			//original 
+			var addOperation = new TwoIntegerOperation(Add);
+			Console.WriteLine("Add result: {0}", addOperation(firstInt, secondInt));
+
+			//C# 2.0 inline function
+			TwoIntegerOperation multiplyOperation = delegate (int a, int b) { return a * b; };
+			Console.WriteLine("Multiply result: {0}", multiplyOperation(firstInt, secondInt));
+
+			//C# 3.0 lambda
+			TwoIntegerOperation unionOperation = (a, b) => (a * 10 + b);
+			Console.WriteLine("Union result: {0}", unionOperation(firstInt, secondInt));
 
 			Console.ReadLine();
 		}
