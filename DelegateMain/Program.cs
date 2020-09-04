f1026e4a6e3fc19bc134edcb1e1017e7f9c5d5ff
Christina.huang <Christina.huang@halliburton.com>
2020-09-04 12:12:22 -0500

    Add project files.

---
+++ DelegateMain/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DelegateMain
+{	
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("please enter two integers");
+
+			var firstInt = int.Parse(Console.ReadLine());
+			var secondInt = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("Add result: {0}", Add(firstInt, secondInt));
+
+			Console.ReadLine();
+		}
+
+		public static int Add(int a, int b)
+		{
+			return a + b;
+		}
+	}
+
+}
